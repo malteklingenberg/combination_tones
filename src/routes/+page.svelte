501846ffb6202a4ac7a22ea6b2ed6ac0579890e4
Malte Klingenberg <malteklingenberg1@gmail.com>
2026-03-31 17:12:26 +0200

    add explainer modal content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,10 @@
 		<h1 class="text-center tracking-wide text-base-content font-header pb-8">Combination Tones</h1>
 
 		<p>
-			don't know what this is about? <button class="btn btn-link p-0" onclick={() => (explainModalOpen = true)}>click here</button> to learn more
+			don't know what this is about? <button
+				class="btn btn-link p-0"
+				onclick={() => (explainModalOpen = true)}>click here</button
+			> to learn more
 		</p>
 
 		<div class="flex w-full flex-wrap justify-center gap-x-18 gap-y-6">
@@ -95,42 +98,50 @@
 >
 	<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
 	<div class="modal-box w-11/12 max-w-5xl" onclick={(e) => e.stopPropagation()}>
-		<h3 class="text-lg font-bold">Hello!</h3>
-		<p class="py-4">
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-			invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-			justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-			ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-			eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-			et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-			sanctus est Lorem ipsum dolor sit amet.
+		<h2 class="text-xl font-bold">Hello!</h2>
+		<p class="py-4 text-sm">
+			This is a demonstration of the acoustic phenomenon of <i>combination tones</i>. When two notes
+			of different frequencies are played, nonlinearities in the ear, but also in electronic
+			speakers and elsewhere generate additional frequencies, which can be heard as extra notes.
 		</p>
-		<p class="py-4">
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-			invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-			justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-			ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-			eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-			et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-			sanctus est Lorem ipsum dolor sit amet.
+
+		<p class="py-4 text-sm">
+			<b>Cool! How do I use it?</b><br />
+			Select two notes on the left. The resulting combination tones are displayed on the right. Play the
+			notes you selected using the play buttons below them and try to hear the combination tones. If you
+			are having trouble discerning them, you can play the combination tones to give you a hint of what
+			to listen out for. You can also switch between just intonation (intervals based on the harmonic
+			series) and equal temperament using the dropdown menu at the top.
 		</p>
-		<p class="py-4">
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-			invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-			justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-			ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-			eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-			et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-			sanctus est Lorem ipsum dolor sit amet.
+
+		<p class="py-4 text-sm">
+			<b>What's up with the cent symbol?</b><br />
+			Sometimes, the combination tones don't align nicely with the scale notes in just intonation. The
+			number below the note gives the size of that deviation: a negative value means that the combination
+			tone is lower than the displayed note, while a positive value means it is higher. The deviation
+			is given in cents, where 1200 cents is an octave and 100 cents an equal-tempered semitone. Note
+			that even if you select equal temperament, the deviation given always refers to the displayed note
+			in just intonation.
 		</p>
-		<p class="py-4">
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-			invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-			justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-			ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-			eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-			et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-			sanctus est Lorem ipsum dolor sit amet.
+
+		<p class="py-4 text-sm">
+			<b>Which frequency ratios did you use to define the just intervals?</b><br />
+			Where possible, I've used the frequency ratios of neighbouring notes on the harmonic series: for
+			the minor second (16:15), major second (9:8), minor third (6:5), major third (5:4), perfect fourth
+			(4:3), perfect fifth (3:2). The minor sixth (8:5), major sixth (5:3) and major seventh (15:8) are
+			also taken directly from the harmonic series. For the minor seventh and augmented fourth/diminished
+			fifth, several definitions are common. I've chosen to define the minor seventh as a minor third
+			above a perfect fifth (9:5), and the diminished fifth as a perfect fourth above a minor second (64:45).
 		</p>
+
+		<p class="py-4 text-sm">
+			<b>I'm looking at this on my phone, and it all looks a bit squished and odd!</b><br />
+			Well, go look at it on a landscape screen instead. I might make this app properly responsive if
+			I find the time.
+		</p>
+
+		<div class="container py-4 min-w-full flex flex-col items-center">
+			<button class="btn btn-primary" onclick={() => (explainModalOpen = false)}>Got it!</button>
+		</div>
 	</div>
 </dialog>
